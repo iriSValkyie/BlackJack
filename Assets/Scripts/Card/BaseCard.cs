16d2857fc4facdc6ba01658bda3f2c468db8bc51
iriSVakjyrie <seiryuu2772@gmail.com>
2022-12-10 04:49:59 +0900

    ある程度予想の付く範囲でスクリプトを作成,カードのプレハブテンプレートを作成

---
+++ Assets/Scripts/Card/BaseCard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackJack
+{
+    public class BaseCard
+    {
+        private CardData Data;
+
+    }
+}
---
+++ Assets/Scripts/Data/Card/CardData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BlackJack
+{
+    public struct CardData
+    {
+        public int Number { get; }
+        public CardType Type { get; }
+        public CardColor Color { get; }
+        public CardData(int num,CardType type,CardColor color)
+        {
+            Number = num;
+            Type = type;
+            Color = color;
+        }
+    }
+    public enum CardType
+    {
+        Spade,
+        Heart,
+        Diamond,
+        Club
+    }
+
+    public enum CardColor
+    {
+        Red,
+        Black,
+    }
+}

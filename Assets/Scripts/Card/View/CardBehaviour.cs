d28cb7fb1d8094ed9e07e78c709df7f290f71fc2
iriSVakjyrie <seiryuu2772@gmail.com>
2022-12-11 20:13:34 +0900

    カードの基本データを追加

--- Assets/Scripts/Card/View/CardBehaviour.cs
+++ Assets/Scripts/Card/View/CardBehaviour.cs
@@ -1,15 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using BlackJack;
 public class CardBehaviour : MonoBehaviour
 {
+    private Card m_card;
+
+    [SerializeField] Text m_Type;
+    [SerializeField] Text m_Num;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
+    public void GenerateCard(string num,CardType type,CardColor color)
+    {
+        m_card = new Card(num,type,color,m_Type,m_Num);
+    }
+
+
+    public void InitCard()
+    {
+        m_card = null;
+        m_Num.text = "";
+        m_Type.text = "";
+    }
     // Update is called once per frame
     void Update()
     {

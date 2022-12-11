f8e80ac277a581ac1813107a0135d4bcf799875c
iriSVakjyrie <seiryuu2772@gmail.com>
2022-12-11 20:13:21 +0900

    カード生成をテストする仮スクリプトを作成

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BlackJack;
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<CardGenerator>().GetCard(transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
